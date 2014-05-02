0c66375349d98227cc03149b32323c400cfeff66
Ajico KIRIHARA <orangepillow@gmail.com>
2014-05-02 13:07:46 +0900

    Use UUID as picture file name for uniqueness

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -45,9 +45,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + File.extname(super) 
-    name.downcase
+    "#{secure_token}.#{file.extension}" if original_filename.present?
   end
 
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
